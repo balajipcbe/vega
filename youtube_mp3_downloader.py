a70a2cbc26f3c45a66901845d937d12c684df54b
Balaji Palaniswami <balajipa@cisco.com>
2019-03-28 22:28:30 -0700

    downloader commit

---
+++ youtube_mp3_downloader.py
@@ -0,0 +1,48 @@
+from pytube import YouTube
+import argparse
+import os
+import subprocess
+
+class YouTubeMp3Downloader:
+    def __init__(self, videoUrl=None, dl_dir=os.getcwd()):
+        self.__url = videoUrl
+        self.__dir = dl_dir
+        self.__ytObj = None
+        self.__ytStreamObj = None
+
+    def downloadYouTube(self):
+        self.__ytObj = YouTube(self.__url)
+        self.__ytStreamObj = self.__ytObj.streams.filter(progressive=True,
+                                            file_extension='mp4'). \
+                                            order_by('resolution'). \
+                                            desc().first()
+        print('Downloading ... '+self.__ytObj.title)
+        self.__ytStreamObj.download(self.__dir)
+
+    def convertToMp3(self):
+        new_filename =  self.__ytStreamObj.default_filename[:
+                        self.__ytStreamObj.default_filename.find('.')]+'.mp3'
+        default_filename =  os.path.join(self.__dir,
+                                         self.__ytStreamObj.default_filename)
+        new_filename = os.path.join(self.__dir, new_filename)
+        subprocess.call(['ffmpeg','-i', default_filename,
+                         new_filename],
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL)
+        subprocess.call(['rm',default_filename])
+        print(new_filename +' downloaded')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="Paste your youtube URL")
+    parser.add_argument("--dir", default=os.getcwd(), help="Download directory\
+                        (default : current directory)")
+    args = parser.parse_args()
+
+    if args.dir and not os.path.exists(args.dir):
+        os.makedirs(args.dir)
+
+    yt = YouTubeMp3Downloader(args.url,args.dir)
+    yt.downloadYouTube()
+    yt.convertToMp3()
